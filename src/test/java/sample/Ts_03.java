2f5cab354e030755d57905d00afc75f59b70e70d
Yashaswini k <yashaswini@getvymo.com>
2023-04-05 13:07:16 +0530

    added ts03

---
+++ src/test/java/sample/Ts_03.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Ts_03 
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("test 01");
+		System.out.println("test 02");
+		System.out.println("test 03");
+		System.out.println("test 04");
+	}
+
+}
